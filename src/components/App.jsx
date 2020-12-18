19ebc3255bd2aefaacbc7c27f2faf4dcbb9a2517
bjimingtang <brendan.jiming.tang@gmail.com>
2020-12-18 06:07:40 -0800

    use the contacts.js file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import Card from "./Card.jsx";
+import contacts from "../contacts.js";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        phone="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name}
+        image={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
       />
       <Card
-        name="Chuck Norris"
-        image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        phone="+918 372 574"
-        email="gmail@chucknorris.com"
+        name={contacts[1].name}
+        image={contacts[1].imgURL}
+        phone={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        image={contacts[2].imgURL}
+        phone={contacts[2].phone}
+        email={contacts[2].email}
       />
     </div>
   );
